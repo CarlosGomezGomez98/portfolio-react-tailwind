fef646dae6b4f32ba26108c35f262608d9af15c7
cgomez <carlosgomezgomez1998@gmail.com>
2025-12-22 13:20:01 +0100

    Navbar, background y base project

---
+++ frontend/src/components/ThemeToggle.jsx
@@ -0,0 +1,37 @@
+import { Sun, Moon } from "lucide-react";
+import { useState, useEffect } from "react";
+import { cn } from "@/lib/utils";
+
+export const ThemeToggle = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        setIsDarkMode(storedTheme === "dark");
+        document.documentElement.classList.toggle("dark", storedTheme === "dark");
+    }, []);
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            setIsDarkMode(false);
+            localStorage.setItem("theme", "light");
+            document.documentElement.classList.remove("dark");
+        } else {
+            setIsDarkMode(true);
+            localStorage.setItem("theme", "dark");
+            document.documentElement.classList.add("dark");
+        }
+
+    };
+    return (
+        <button onClick={toggleTheme} className={cn(
+            "fixed max-sm:hidden top-5 right-5 z-50 rounded-full transition-colors duration-300",
+             "focus:outline-none"
+        )}>
+            { isDarkMode ? 
+                (<Sun className=" h-6 w-6 text-yellow-300" />) : 
+                (<Moon className=" h-6 w-6 text-blue-900" />)
+            }
+        </button>
+    );
+};
