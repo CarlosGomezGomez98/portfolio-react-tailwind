6e57152db3cb373ea5f7a0176ab1be8a24a585ea
cgomez <carlosgomezgomez1998@gmail.com>
2025-12-23 16:17:08 +0100

    cambio en el hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
             <div className="container max-w-4xl mx-auto text-center z-10">
                 <div className="space-y-6">
                     <h1 className="text-4xl md:text-6xl font-bold tracking-tighter">
-                        <span className="opacity-0 animate-fade-in"> Hola, soy</span>
+                        <span className="opacity-0 animate-fade-in"> Hey, I'm</span>
                         <span className="text-primary opacity-0 animate-fade-in-delay-1">{" "}Carlos </span>
                         <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">{" "}Gómez</span>
                     </h1>
